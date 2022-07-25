736580c79bef6ea5f19f0f1f1fec61c554568707
saleszera <crowzcoleslaw@gmail.com>
2022-07-25 20:56:53 -0300

    Add more tests into form and global states with recoilJs

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,14 +1,114 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import { RecoilRoot } from 'recoil'
 
 import { Form } from '.'
 
-test('Then the input is empty, new participants shouldn\'t be added', () => {
-  render(<Form />)
-
-  const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-
-  const button = screen.getByRole('button')
-
-  expect(input).toBeInTheDocument()
-  expect(button).toBeDisabled()
+describe('Form behavior 🚀️', () => {
+  it('shouldn\'t add participants if input is empty', () => {
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+  
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  
+    const button = screen.getByRole('button')
+  
+    expect(input).toBeInTheDocument()  
+    expect(button).toBeDisabled()
+  })
+  
+  it('Should be add a participant if there is a name filled in', () => {
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+  
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  
+    const button = screen.getByRole('button')
+  
+    fireEvent.change(input, {
+      target: {
+        value: 'Raniery Sales'
+      }
+    })
+  
+    fireEvent.click(button)
+  
+    expect(input).toHaveFocus()
+    expect(input).toHaveValue('')
+  })
+  
+  it('Shouldn\'t add a duplicated name', () => {
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+  
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  
+    const button = screen.getByRole('button')
+  
+    fireEvent.change(input, {
+      target: {
+        value: 'Raniery Sales'
+      }
+    })
+  
+    fireEvent.click(button)
+    fireEvent.change(input, {
+      target: {
+        value: 'Raniery Sales'
+      }
+    })
+  
+    fireEvent.click(button)
+  
+    const messageError = screen.getByRole('alert')
+    expect(messageError.textContent).toBe('Nomes duplicados não são permitidos!')
+  })
+  
+  it('Error message should be lost after three minutes', () => {
+    jest.useFakeTimers()
+    render(
+      <RecoilRoot>
+        <Form />
+      </RecoilRoot>
+    )
+  
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  
+    const button = screen.getByRole('button')
+  
+    fireEvent.change(input, {
+      target: {
+        value: 'Raniery Sales'
+      }
+    })
+  
+    fireEvent.click(button)
+    fireEvent.change(input, {
+      target: {
+        value: 'Raniery Sales'
+      }
+    })
+  
+    fireEvent.click(button)
+  
+    let messageError = screen.queryByRole('alert')
+    expect(messageError).toBeInTheDocument() 
+  
+    act(() => {
+      jest.runAllTimers()
+    })  
+  
+    messageError = screen.queryByRole('alert')
+    expect(messageError).toBeNull()
+  })
 })
+
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,33 @@
-export const Form = () => (
-  <form>
-    <input type="text" placeholder='Insira os nomes dos participantes' />
-    <button type='button' disabled>Adicionar</button>
-  </form>
-)
+import {useRef, FormEvent, useCallback, useState} from 'react'
+import { useAddParticipants } from '../../state/hooks/useAddParticipants'
+import { useErrorMessage } from '../../state/hooks/useErrorMessage'
+
+export const Form = () => {
+  const [name, setName] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)  
+
+  const addParticipantIntoList = useAddParticipants()
+  const errorMessage = useErrorMessage()
+
+  const handleAddParticipant = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    addParticipantIntoList(name)
+    setName('')
+    inputRef.current?.focus()
+  }, [inputRef, addParticipantIntoList, name])
+
+  return (
+    <form onSubmit={handleAddParticipant}>
+      <input 
+        ref={inputRef} 
+        type="text"
+        placeholder='Insira os nomes dos participantes'
+        value={name} 
+        onChange={event => setName(event.target.value)}
+      />
+      <button type="submit" disabled={!name}>Adicionar</button>
+      {errorMessage && <p role="alert">{errorMessage}</p>}
+    </form>
+  )
+}
